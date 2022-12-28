d2ebfda8d67c516116479efb161ec18e276b94f1
sula92 <sulakkanaid@gmail.com>
2022-12-29 03:16:49 +0530

    branches cors

--- src/main/java/com/sula/ranjith_learners/controller/BranchController.java
+++ src/main/java/com/sula/ranjith_learners/controller/BranchController.java
@@ -10,14 +10,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController//(@controller+@ResponseBody)
-@RequestMapping("/api")
+@RequestMapping(
+        value = "/api",
+        produces = "application/json")
+@Transactional
+
+@CrossOrigin(origins = {
+        "*"
+
+},
+        allowedHeaders = "*",
+
+        maxAge = 15 * 60,
+        methods = {
+                RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.DELETE,
+                RequestMethod.PUT
+        })
 public class BranchController {
 
     @Autowired
